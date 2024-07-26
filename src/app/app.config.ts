6054c1463955ca68f6be789841e619653327d519
Yuri Beneche <ybenech@wgu.edu>
2024-07-26 10:05:24 -0400

    fixes missing config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    importProvidersFrom(BrowserModule),
+    provideHttpClient()
+  ]
 };
