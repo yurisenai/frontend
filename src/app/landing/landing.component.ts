12c6e8f5ef988cb1a1c5ca13eebd47f7bbb90ad1
Noah Chun <78376355+noahchun@users.noreply.github.com>
2024-08-01 18:28:18 -0400

    dynamic landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MapsComponent } from '../maps/maps.component';
 import { NgFor, NgIf } from '@angular/common';
+import { Employee } from '../models/employee';
+import { HttpService } from '../services/http.service';
+import { Clearance, Project } from '../models/project';
 
 
 @Component({
@@ -13,26 +16,85 @@ import { NgFor, NgIf } from '@angular/common';
 export class LandingComponent {
   isProjectView = true;
 
-  employees = [
-    { pfp: "./assets/images/person3.png", name: 'James Brown', location: 'New York' },
-    { pfp: "./assets/images/person2.png", name: 'Sophia Martinez', location: 'Los Angeles' },
-    { pfp: "./assets/images/person1.png", name: 'Alyssa Johnson', location: 'Off World' },
-    { pfp: "./assets/images/person4.png", name: 'Michael Anderson', location: 'On Base' },
+  employees: Employee[] = [];
 
-  ];
+  constructor(private httpService: HttpService){
+    this.getAllEmployees();
+    this.getAllProjects();
+  }
+
+  getAllEmployees(){
+    this.httpService.getAllEmployees().subscribe(response => {
+      if (response && response.body) {
+        this.employees = [];
+
+        let body: any = response.body || {}
+        for (let item of body) {
+          this.employees.push(new Employee(item.id, item.firstName,
+             item.lastName,item.email, item.phoneNumber, item.occupation,item.clearance,item.img, item.projects,item.location));
+
+        }
+      }
+    });
+  }
 
   toggleView() {
     this.isProjectView = !this.isProjectView;
   }
 
 
-  projects = [
-    { name: 'Imperial Welcome', staff: { count: 40}, completion: { percent: 77 } },
-    { name: 'Medical Exchanges', staff: { count: 16}, completion: { percent: 75 } },
-    { name: 'Space Lasers', staff: { count: 49 }, completion: {percent: 188 } },
-    { name: 'Reverse Engineer UAP', staff: { count: 50 }, completion: { percent: 70 } },
-    { name: 'ET Translation System', staff: { count: 30 }, completion: { percent: 45 } },
-
+  projects: Project[] = [
+    new Project(
+      1,
+      'Reverse Engineer Ship',
+      'Analyze alien technology and recreate it.',
+      new Clearance(1, 'Top Secret', []),
+      'High',
+      50,
+      '',
+      [],
+    ),
+    new Project(
+      2,
+      'Alien Translation System',
+      'Analyze alien language and translate it.',
+      new Clearance(2, 'Secret', []),
+      'Middle',
+      35,
+      '',
+      [],
+    ),
+    new Project(
+      3,
+      'Test Laser Firearms',
+      'Understand the mechanisms of alien technology.',
+      new Clearance(3, 'Top Secret', []),
+      'Middle',
+      20,
+      '',
+      [],
+    )
   ];
+
+  getAllProjects() {
+    this.httpService.getAllProjects().subscribe(response => {
+      if (response && response.body) {
+        this.projects = []; 
+        let body: any = response.body || [];
+        for (let item of body) {
+          this.projects.push(new Project(
+            item.id,
+            item.codename,
+            item.description,
+            new Clearance(item.minClearance.id, item.minClearance.clearance, item.minClearance.employees),  
+            item.priority,
+            item.personnel,
+            item.img,
+            item.employees,
+          ));
+        }
+      }
+    });
+  }
 
 }
