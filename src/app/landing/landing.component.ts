fba132cb424550f2daf6d3f75e1f8dfae3cc66ef
Noah Chun <78376355+noahchun@users.noreply.github.com>
2024-08-05 16:04:31 -0400

    crud functionality

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,102 +5,60 @@ import { Employee } from '../models/employee';
 import { HttpService } from '../services/http.service';
 import { Clearance, Project } from '../models/project';
 
-
-
-
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [MapsComponent, NgFor, NgIf],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
   isProjectView = true;
 
   employees: Employee[] = [];
+  projects: Project[] = [];
 
-  constructor(private httpService: HttpService){
+  constructor(private httpService: HttpService) {
     this.getAllEmployees();
     this.getAllProjects();
   }
 
-  getAllEmployees(){
+  getAllEmployees() {
     this.httpService.getAllEmployees().subscribe(response => {
-      if (response && response.body) {
-        this.employees = [];
-
-        let body: any = response.body || {}
-        for (let item of body) {
-          this.employees.push(new Employee(item.id, item.firstName,
-             item.lastName,item.email, item.phoneNumber, item.occupation,item.clearance,item.img, item.projects,item.location));
-
-        }
-      }
+      // if (response) {
+      //   this.employees = response.map(item => new Employee(
+      //     item.id,
+      //     item.firstName,
+      //     item.lastName,
+      //     item.email,
+      //     item.phoneNumber,
+      //     item.occupation,
+      //     item.clearance,
+      //     item.img,
+      //     item.projects,
+      //     item.location
+      //   ));
+      // }
     });
   }
-
 
   toggleView() {
     this.isProjectView = !this.isProjectView;
   }
 
-
-  projects: Project[] = [
-    new Project(
-      1,
-      'Reverse Engineer Ship',
-      'Analyze alien technology and recreate it.',
-      new Clearance(1, 'Top Secret', []),
-      'High',
-      50,
-      '',
-      [],
-    ),
-    new Project(
-      2,
-      'Alien Translation System',
-      'Analyze alien language and translate it.',
-      new Clearance(2, 'Secret', []),
-      'Middle',
-      35,
-      '',
-      [],
-    ),
-    new Project(
-      3,
-      'Test Laser Firearms',
-      'Understand the mechanisms of alien technology.',
-      new Clearance(3, 'Top Secret', []),
-      'Middle',
-      20,
-      '',
-      [],
-    )
-  ];
-
-
-
   getAllProjects() {
     this.httpService.getAllProjects().subscribe(response => {
-      if (response && response.body) {
-        this.projects = [];
-        let body: any = response.body || [];
-        for (let item of body) {
-          this.projects.push(new Project(
-            item.id,
-            item.codename,
-            item.description,
-            new Clearance(item.minClearance.id, item.minClearance.clearance, item.minClearance.employees),
-            item.priority,
-            item.personnel,
-            item.img,
-            item.employees,
-          ));
-
-        }
-      }
+      this.projects = response.map(item => new Project(
+        item.id,
+        item.codename,
+        item.description,
+        new Clearance(item.minClearance.clearanceLevel, item.minClearance.clearanceType, item.minClearance.employees),
+        item.priority,
+        item.personnel,
+        item.img,
+        item.employees
+      ));
     });
   }
-
+  
 }
